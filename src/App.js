fa19f35b6871206a05a311f6b46af65cafc0eb67
redips <redips@prismweaver.com>
2022-05-12 23:07:05 +0900

    [React study] Themes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
-import styled, {keyframes} from 'styled-components';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: ${props => props.theme.textColor}
+`;
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const animation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  animation: ${animation} 1s linear infinite;
-  display: flex;
-  align-items: center;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
-
-  ${Emoji}:hover {
-    font-size: 80px;
-  }
-  
+  align-items: center;
+  background-color: ${props => props.theme.backgroundColor};
 `;
 
 function App() {
     return (
         <Wrapper>
-            <Box>
-                <Emoji as={"p"}>😘</Emoji>
-            </Box>
-            <Emoji as={"p"}>👀</Emoji>
+            <Title>Redips React Study</Title>
         </Wrapper>
     );
 }
