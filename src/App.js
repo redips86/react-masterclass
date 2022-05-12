84231c7a464c9fa80882d35a6680b0c51d1440ed
redips <redips@prismweaver.com>
2022-05-12 19:11:30 +0900

    [React study] Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -4,29 +4,22 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`
-
-const Text = styled.span`
-  color:white;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `
 
 function App() {
     return (
         <Father>
-            <BoxOne>
-                <Text>Hello</Text>
-            </BoxOne>
-            <BoxTwo/>
+            <Box bgColor={"teal"}/>
+            <Box bgColor={"tomato"}/>
+            <Circle bgColor={"tomato"}/>
         </Father>
     );
 }
