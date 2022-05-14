bb3cfb0934940c491c70f3522943749162596e8f
redips <redips@prismweaver.com>
2022-05-14 13:20:07 +0900

    [React study] Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import Circle from "./Circle";
+import React, {useState} from "react";
 
 function App() {
+    const [value, setValue] = useState("");
+    const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+        const {currentTarget: {value}} = event;
+        setValue(value);
+    };
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log("hello", value);
+    };
+
     return (
         <div>
-            <Circle bgColor="teal" borderColor={"yellow"}></Circle>
-            <Circle bgColor="tomato" text={"override text"}></Circle>
+            <form action="" onSubmit={onSubmit}>
+                <input type="text" placeholder={"username"} onChange={onChange} value={value}/>
+                <button>Log In</button>
+            </form>
         </div>
     );
 }
