d5d923b5ce8df9ec34789b3f6226b4c2540a8511
redips <redips@prismweaver.com>
2022-05-15 10:14:13 +0900

    [React study] 3.7

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 
-function App() {
-    const Container = styled.div`
-      background-color: ${props => props.theme.bgColor}
-    `;
+interface DummyProps {
+    text: string;
+    otherThingHere?: string;
+}
+
+function Dummy({text, otherThingHere = 'TTT'}: DummyProps) {
     const H1 = styled.h1`
       color: ${props => props.theme.textColor}
     `;
 
     return (
+        <>
+            <H1>{text}</H1>
+            <H1>{otherThingHere}</H1>
+        </>
+    );
+}
+
+function App() {
+    const Container = styled.div`
+      background-color: ${props => props.theme.bgColor}
+    `;
+
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        alert(event.currentTarget.value);
+    };
+
+    return (
         <div>
             <Container>
-                <H1>protected</H1>
+                <Dummy text={"redips study"}></Dummy>
+                <button onClick={onClick} value={"tt"}>Click me</button>
             </Container>
         </div>
     );
