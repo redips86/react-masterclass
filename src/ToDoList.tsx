e36398fa3e4fc40f98f49c2ecbdab1f9cffd8de8
redips <redips@prismweaver.com>
2022-05-21 21:56:17 +0900

    [React study] react-hook-form 도입

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,14 +1,33 @@
-import {useState} from "react";
+import {useForm} from "react-hook-form";
 
 function ToDoList() {
+    const {register, watch} = useForm();
+    console.log(watch())
+    return (
+        <div>
+            <form action="">
+                <input {...register("toDo")} type="text" placeholder={"Write a to do"}/>
+                <button>Add</button>
+            </form>
+        </div>
+    );
+}
+
+/*
+function ToDoList() {
+    useForm()
     const [toDo, setToDo] = useState("");
+    const [toDoError, setToDoError] = useState("");
     const onChange = (event: React.FormEvent<HTMLInputElement>) => {
         const {currentTarget: {value}} = event;
+        setToDoError("");
         setToDo(value);
     };
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        if(toDo.length < 10)
+            return setToDoError("To do should be longed");
         console.log(toDo);
     }
 
@@ -17,9 +36,11 @@ function ToDoList() {
             <form action="" onSubmit={onSubmit}>
                 <input onChange={onChange} value={toDo} type="text" placeholder={"Write a to do"}/>
                 <button>Add</button>
+                {toDoError !== "" ? toDoError : null }
             </form>
         </div>
     );
 }
+*/
 
 export default ToDoList;
