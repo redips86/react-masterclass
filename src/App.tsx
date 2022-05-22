2b1f216ad85687b5a5e9eb013798b7d162add26a
redips <redips@prismweaver.com>
2022-05-22 23:54:41 +0900

    [React study] recoil set selector 연습, 실무할때는 minute, hour state 그냥 하나씩 만들고 처리할 것 같음. 그 뒤 리팩토링 할 때 selector 있었지 하고 넘어갈 것 같네

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import {useRecoilState, useRecoilValue} from "recoil";
+import {useRecoilState} from "recoil";
 import {hourSelector, minuteState} from "./atoms";
 
 function App() {
 
     const [minutes, setMinutes] = useRecoilState(minuteState);
-    const hours = useRecoilValue(hourSelector)
+    const [hours, setHours] = useRecoilState(hourSelector);
 
     const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
         setMinutes(+event.currentTarget.value);
     }
 
+    const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+        setHours(+event.currentTarget.value);
+    }
 
     return (
         <div>
             <input type="number" onChange={onMinutesChange} placeholder={"minute"} value={minutes}/>
-            <input type="number" placeholder={"Hours"} value={hours}/>
+            <input type="number" onChange={onHoursChange} placeholder={"Hours"} value={hours}/>
         </div>
     );
 }
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -3,12 +3,16 @@ import {atom, selector} from "recoil";
 export const minuteState = atom({
     key: "minute",
     default: 0,
-})
+});
 
-export const hourSelector = selector({
+export const hourSelector = selector<number>({
     key: "hours",
     get: ({get}) => {
         const minutes = get(minuteState);
         return minutes / 60;
+    },
+    set: ({set}, newValue) => {
+        const minutes = Number(newValue) * 60;
+        set(minuteState, minutes);
     }
 })
