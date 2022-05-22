886c359c03270f5e6f42191ca8d7d91865c0f1c3
redips <redips@prismweaver.com>
2022-05-22 15:46:39 +0900

    [React study] ES6 ES6 ES6

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,20 @@
-import {IToDo} from "../atoms";
+import {IToDo, toDoState} from "../atoms";
+import {useSetRecoilState} from "recoil";
 
-function ToDo({text}: IToDo) {
+function ToDo({text, category, id}: IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+
+    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const {currentTarget: {name}} = event;
+
+        console.log("i wanna to ", name);
+    };
     return (
         <li>
             {text}
-            <button>ToDo</button>
-            <button>Doing</button>
-            <button>Done</button>
+            {category !== 'DOING' && <button name={"DOING"} onClick={onClick}>Doing</button>}
+            {category !== 'TO_DO' && <button name={"TO_DO"} onClick={onClick}>To Do</button>}
+            {category !== 'DONE' && <button name={"DONE"} onClick={onClick}>Done</button>}
         </li>
     );
 }
