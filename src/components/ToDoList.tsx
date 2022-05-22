d707359a5e55a8ad98e1e38175335c40846393b7
redips <redips@prismweaver.com>
2022-05-22 22:36:01 +0900

    [React study] recoil selector

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,20 +1,31 @@
 import {useRecoilValue} from "recoil";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
-import {toDoState} from "../atoms";
+import {toDoSelector} from "../atoms";
 
 
 function ToDoList() {
-    const toDos = useRecoilValue(toDoState);
-    console.log(toDos);
+    const [toDos, doing, done] = useRecoilValue(toDoSelector);
 
     return (
         <div>
             <h1>To Dos</h1>
             <hr/>
             <CreateToDo/>
+            <h2>To Do</h2>
+            <hr/>
             <ul>
                 {toDos.map(todo => <ToDo key={todo.id} {...todo} />)}
+            </ul>
+            <h2>Doing</h2>
+            <hr/>
+            <ul>
+                {doing.map(todo => <ToDo key={todo.id} {...todo} />)}
+            </ul>
+            <h2>Done</h2>
+            <hr/>
+            <ul>
+                {done.map(todo => <ToDo key={todo.id} {...todo} />)}
             </ul>
         </div>
     );
