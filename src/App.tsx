19e4dba1bfe6bbee6178c79cc96715b12b217028
redips <redips@prismweaver.com>
2022-05-24 20:15:47 +0900

    [React study] 애니메이션 맛집4

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-import {motion, useMotionValue, useTransform} from "framer-motion";
-import {useEffect} from "react";
+import {motion, useMotionValue, useTransform, useViewportScroll} from "framer-motion";
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -20,18 +19,27 @@ const Box = styled(motion.div)`
 
 function App() {
     const x = useMotionValue(0);
-    const scale = useTransform(x, [-800, 0, 800], [2, 1, 0]);
+    const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+    const gradient = useTransform(x, [-800, 0, 800], [
+        "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+        "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+        "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 238))"
+    ]);
+    const {scrollYProgress} = useViewportScroll();
+    const scale = useTransform(scrollYProgress, [0, 1], [1, 5])
 
-    useEffect(() => {
-        x.onChange(() => {
-            console.log(scale.get())
-        })
-    }, [x])
+    // useEffect(() => {
+    //     scrollY.onChange(() => {
+    //         console.log(scrollY.get(), scrollYProgress.get())
+    //     })
+    // }, [scrollY, scrollYProgress])
 
     return (
-        <Wrapper>
+        <Wrapper
+            style={{background: gradient}}
+        >
             <Box
-                style={{x, scale}}
+                style={{x, rotateZ, scale}}
                 drag={"x"}
                 dragSnapToOrigin
             />
