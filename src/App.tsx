811a7c917b5f3220abe18b2ae791797059d29a88
redips <redips@prismweaver.com>
2022-05-24 12:37:39 +0900

    [React study] variant 1

--- src/App.tsx
+++ src/App.tsx
@@ -17,18 +17,21 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+    start: {scale: 0},
+    end: {
+        scale: 1, rotateZ: 360, transition: {
+            type: "spring",
+            stiffness: 260,
+            damping: 20
+        }
+    }
+}
+
 function App() {
     return (
         <Wrapper>
-            <Box
-                initial={{scale: 0}}
-                animate={{rotate: 180, scale: 1}}
-                transition={{
-                    type: "spring",
-                    stiffness: 260,
-                    damping: 20
-                }}
-            />
+            <Box variants={myVars} initial={"start"} animate={"end"}/>
         </Wrapper>
     );
 }
