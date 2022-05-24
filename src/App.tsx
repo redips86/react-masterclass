16e9b5b013bdb9ff7eed3f7ccd269fa22cb8c959
redips <redips@prismweaver.com>
2022-05-24 21:00:09 +0900

    [React study] slider

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 const Box = styled(motion.div)`
   width: 400px;
@@ -15,44 +16,61 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   position: absolute;
+  display: flex;
   top: 100px;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-    initial: {
+const box = {
+    invisible: {
+        x: 500,
         opacity: 0,
         scale: 0
     },
     visible: {
+        x: 0,
         opacity: 1,
         scale: 1,
-        rotateZ: 360
+        transition: {
+            duration: 1
+        }
     },
-    leaving: {
+    exit: {
+        x: -500,
         opacity: 0,
         scale: 0,
-        y: 50
+        transition: {
+            duration: 1
+        }
     }
 }
 
 function App() {
-    const [showing, setShowing] = useState(false);
-    const toggleShowing = () => {
-        setShowing(!showing);
-    }
+    const [visible, setVisible] = useState(1);
+    const nextPlease = () => setVisible(prev => prev === 10 ? 10 : prev + 1);
+    const prevPlease = () => setVisible(prev => prev === 0 ? 0 : prev - 1);
+
     return (
         <Wrapper>
-            <button onClick={toggleShowing}>Click</button>
             <AnimatePresence>
-                {showing
-                    ? <Box
-                        variants={boxVariants}
-                        animate={"visible"}
-                        exit={"leaving"}
-                        initial={"initial"}/>
-                    : null}
+                {
+                    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(
+                        (i) =>
+                            i === visible ?
+                                <Box
+                                    variants={box}
+                                    initial={"invisible"}
+                                    animate={"visible"}
+                                    exit={"exit"}
+                                    key={i}>{i}</Box> : null
+                    )
+                }
             </AnimatePresence>
+            <button onClick={nextPlease}>next</button>
+            <button onClick={prevPlease}>prev</button>
         </Wrapper>
     );
 }
