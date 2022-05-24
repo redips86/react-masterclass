f57255fd2201f0abd8347643411c87e14a26e2ce
redips <redips@prismweaver.com>
2022-05-24 21:42:22 +0900

    [React study] wow

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,46 @@
 import styled from "styled-components";
-import {AnimatePresence, motion} from "framer-motion";
+import {motion} from "framer-motion";
 import {useState} from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
 `;
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
   display: flex;
-  top: 100px;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const box = {
-    entry: (back: boolean) => ({
-        x: back ? -500 : 500,
-        opacity: 0,
-        scale: 0
-    }),
-    center: {
-        x: 0,
-        opacity: 1,
-        scale: 1,
-        transition: {
-            duration: 0.3
-        }
-    },
-    exit: (back: boolean) => ({
-        x: back ? 500 : -500,
-        opacity: 0,
-        scale: 0,
-        transition: {
-            duration: 0.3
-        }
-    })
-}
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
-    const [visible, setVisible] = useState(1);
-    const [back, setBack] = useState(false);
-    const nextPlease = () => {
-        setBack(false);
-        setVisible(prev => prev === 10 ? 10 : prev + 1);
-    };
-    const prevPlease = () => {
-        setBack(true);
-        setVisible(prev => prev === 1 ? 1 : prev - 1)
-    };
+    const [clicked, setClicked] = useState(false);
+    const toggleClicked = () => setClicked((prev => !prev))
 
     return (
-        <Wrapper>
-            {/*<AnimatePresence exitBeforeEnter>*/}
-            <AnimatePresence>
-                {
-                    <Box
-                        custom={back}
-                        variants={box}
-                        initial={"entry"}
-                        animate={"center"}
-                        exit={"exit"}
-                        key={visible}>{visible}</Box>
-                }
-            </AnimatePresence>
-            <button onClick={nextPlease}>next</button>
-            <button onClick={prevPlease}>prev</button>
+        <Wrapper onClick={toggleClicked}>
+            <Box>
+                {!clicked ? <Circle layoutId={"circle"} style={{borderRadius: 50}}/> : null}
+            </Box>
+            <Box>
+                {clicked ? <Circle layoutId={"circle"} style={{borderRadius: 0, scale: 2}}/> : null}
+            </Box>
         </Wrapper>
     );
 }
