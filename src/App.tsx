bb4d115c9044aeafeb01325b717cd191507e8981
redips <redips@prismweaver.com>
2022-05-24 13:19:34 +0900

    [React study] 애니메이션 맛집

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import {motion} from "framer-motion";
+import {useRef} from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,22 +10,26 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  backgroud-color: rgba(255, 255, 255, 0.4);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 15px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  width: 70px;
-  height: 70px;
-  background-color: white;
-  place-self: center;
-  border-radius: 35px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
 
 const boxVariants = {
     hover: {scale: 1.5, rotateZ: 90},
@@ -39,13 +44,20 @@ const boxVariants = {
 
 
 function App() {
+    const biggerBoxRef = useRef<HTMLDivElement>(null);
     return (
         <Wrapper>
-            <Box drag
-                 variants={boxVariants}
-                 whileDrag={"drag"}
-                 whileHover={"hover"}
-                 whileTap={"click"}/>
+            <BiggerBox ref={biggerBoxRef}>
+                <Box
+                    drag
+                    dragSnapToOrigin
+                    dragElastic={0.5}
+                    dragConstraints={biggerBoxRef}
+                    variants={boxVariants}
+                    whileDrag={"drag"}
+                    whileHover={"hover"}
+                    whileTap={"click"}/>
+            </BiggerBox>
         </Wrapper>
     );
 }
