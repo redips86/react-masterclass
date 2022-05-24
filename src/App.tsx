6e36df4afa5ea81b76a40d9a206f202064d3fdaa
redips <redips@prismweaver.com>
2022-05-24 12:32:52 +0900

    [React study] framer 연습임. 특별한 챕터는 아닌것 같다.

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,26 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
     return (
         <Wrapper>
-            <Box/>
-            <div></div>
-            <motion.div></motion.div>
+            <Box
+                initial={{scale: 0}}
+                animate={{rotate: 180, scale: 1}}
+                transition={{
+                    type: "spring",
+                    stiffness: 260,
+                    damping: 20
+                }}
+            />
         </Wrapper>
     );
 }
