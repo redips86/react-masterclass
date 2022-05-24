01329b9ecdcb4386a5ef11310b27aae9d0853753
redips <redips@prismweaver.com>
2022-05-24 19:53:58 +0900

    [React study] 애니메이션 맛집2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import {motion} from "framer-motion";
-import {useRef} from "react";
+import {motion, useMotionValue} from "framer-motion";
+import {useEffect} from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,19 +9,6 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  backgroud-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-`;
-
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
@@ -31,33 +18,22 @@ const Box = styled(motion.div)`
 `;
 
 
-const boxVariants = {
-    hover: {scale: 1.5, rotateZ: 90},
-    click: {scale: 1.0, borderRadius: "180px"},
-    drag: {
-        backgroundColor: "rgb(46, 204,113)",
-        transition: {
-            duration: 10
-        }
-    }
-}
-
-
 function App() {
-    const biggerBoxRef = useRef<HTMLDivElement>(null);
+    const x = useMotionValue(0);
+    useEffect(() => {
+        x.onChange(() => {
+            console.log(x.get())
+        })
+    }, [x])
+    console.log(x);
     return (
         <Wrapper>
-            <BiggerBox ref={biggerBoxRef}>
-                <Box
-                    drag
-                    dragSnapToOrigin
-                    dragElastic={0.5}
-                    dragConstraints={biggerBoxRef}
-                    variants={boxVariants}
-                    whileDrag={"drag"}
-                    whileHover={"hover"}
-                    whileTap={"click"}/>
-            </BiggerBox>
+            <button onClick={() => x.set(200)}>cccc</button>
+            <Box
+                style={{x}}
+                drag={"x"}
+                dragSnapToOrigin
+            />
         </Wrapper>
     );
 }
