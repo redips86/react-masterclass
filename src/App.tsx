ddbbeecfbe48bd9aea9f79668eeb17b3a4f03750
redips <redips@prismweaver.com>
2022-05-25 10:36:59 +0900

    [React study] header2

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
         <BrowserRouter>
             <Header/>
             <Routes>
-                <Route path={"/"} element={<Home/>}></Route>
                 <Route path={"/tv"} element={<Tv/>}></Route>
                 <Route path={"/search"} element={<Search/>}></Route>
+                <Route path={"/"} element={<Home/>}></Route>
             </Routes>
         </BrowserRouter>
     );
